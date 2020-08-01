daa86ffb7f8f1c69122fd1105ef859ef2d7f1b57
Lehlogonolo N. Masubelele <codesenju@gmail.com>
2020-08-01 12:21:35 +0200

    Update example.tf

--- example.tf
+++ example.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_key_pair" "deployer" {
 key_name = "deploy_key"
 // enter the public key below
-public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCwLPO7LRZNdB84pZDgKqzcA04lhG20lRsZLdN4bQxVC98tFp1aiEigOKrMvDP2U/ZSN7e4nYgN4iTl56QC+CE1egMuUy0jYznZLiZ94+9KzdGL9KzAmFvj8ZUaLo9VClNNBHKjTQKGcI/TmPOPqxZisn2K26qtoW15w4XB2Tm6SrSbz91VTHkHblyeQl5v/1H3cV6BDh1vnoOgSdV+UFSML7T7to7eJjNa/51ifH9Ahma0sA1tXMr3BJ6A0PFy83aciXpxyyvn58zVl+evXZhLNzwxZ3hw3uiJm14nCaz3N0txvz/eoClqtnEvL2YttmR4iIDapqYV/1uu4i+lP2N/kQI0E7j+y7xNXp+Af7LNbr948PtSEjNs2mchtBSszjZSi6xZnQd/a0pUwAMpdppn1QPo5Lp1S5k3m2SD83nK7kWkI2TGR9XhxgJGS/vtkeXoj3KkdDbKGBH44xRrroZoXQYNFovNYbsLKFxjnrvGYmCKK9jaJJbJtX7DOLP2KFm1DLnof89PP7aZMKBcc1rfigUsNUvfL6hjm1xp3+FMCJvO/TECg3hGBsE9X2uy7znNZhVqlK/ilaAydaqtYBtWu7yXQbTHS2Tf7vbC0mpzykHldgWbfXAx3WbWBwjZ7D/X7Cnl3ssizYfxINKEJiaSm+Vsc8lR4DFSOUChBrz9PQ== Lehlogonolo.Masubelele@ibm.com"
+public_key = ""
 }
 
 // security group creation
